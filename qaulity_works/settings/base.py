f2f15d28449539f5f5aaca786aa3cb0f937afc7a
godhanidenis <godhanidenis@gmail.com>
2021-10-18 10:07:15 +0530

    sqlite database

--- qaulity_works/settings/base.py
+++ qaulity_works/settings/base.py
@@ -61,12 +61,12 @@ CORS_ORIGIN_WHITELIST = (
 )
 CORS_ALLOW_CREDENTIALS = True
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 TEMPLATES = [
     {
@@ -135,4 +135,4 @@ REST_FRAMEWORK={
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
